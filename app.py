7060c7ff39458f7814549865bc3038936c0b19b4
Victoria Demina <viktoriia.demina@gmail.com>
2023-04-04 11:04:11 +0100

    Add reading statuses and some styling.

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ todos = db.todos
 @app.route('/', methods=('GET', 'POST'))
 def index():
     if request.method == 'POST':
-        content = request.form['content']
-        degree = request.form['degree']
-        todos.insert_one({'content': content, 'degree': degree})
+        title = request.form['title']
+        status = request.form['status']
+        todos.insert_one({'title': title, 'status': status})
         return redirect(url_for('index'))
     all_todos = todos.find()
     return render_template('index.html', todos=all_todos)
