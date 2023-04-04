ae048086a91d0116328bfada0d59de1fc45fb16a
Victoria Demina <viktoriia.demina@gmail.com>
2023-04-04 11:15:46 +0100

    Refactoring.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 
 db = client.flask_db
-todos = db.todos
+books = db.books
 
 
 @app.route('/', methods=('GET', 'POST'))
@@ -15,13 +15,13 @@ def index():
     if request.method == 'POST':
         title = request.form['title']
         status = request.form['status']
-        todos.insert_one({'title': title, 'status': status})
+        books.insert_one({'title': title, 'status': status})
         return redirect(url_for('index'))
-    all_todos = todos.find()
-    return render_template('index.html', todos=all_todos)
+    all_books = books.find()
+    return render_template('index.html', books=all_books)
 
 
 @app.post('/<id>/delete/')
 def delete(id):
-    todos.delete_one({"_id": ObjectId(id)})
+    books.delete_one({"_id": ObjectId(id)})
     return redirect(url_for('index'))
